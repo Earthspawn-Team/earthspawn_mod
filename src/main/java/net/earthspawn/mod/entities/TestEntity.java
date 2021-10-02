bfd0afcde9c87c7573d5d9278a67855880124846
Earthspawn <earthspawn.team@gmail.com>
2021-10-02 17:17:06 +0200

    Chipser

--- src/main/java/net/earthspawn/mod/entities/TestEntity.java
+++ src/main/java/net/earthspawn/mod/entities/TestEntity.java
@@ -1,14 +1,17 @@
 package net.earthspawn.mod.entities;
 
+import net.earthspawn.mod.EarthspawnMod;
 import net.earthspawn.mod.utils.RegistryHandler;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.AgeableEntity;
+import net.minecraft.entity.CreatureAttribute;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.SwimGoal;
 import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.network.IPacket;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
@@ -17,6 +20,10 @@ import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 import javax.annotation.Nullable;
 
@@ -26,18 +33,17 @@ public class TestEntity extends AnimalEntity {
         super(type, worldIn);
     }
 
-    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
-        return MobEntity.func_233666_p_()
-                .createMutableAttribute(Attributes.MAX_HEALTH, 10.0D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3D)
-                .createMutableAttribute(Attributes.ATTACK_DAMAGE, 0.1D);
-    }
-
     @Override
     protected void registerGoals() {
         super.registerGoals();
 
         this.goalSelector.addGoal(0, new SwimGoal(this));
+    }
+
+    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
+        return MobEntity.func_233666_p_()
+                .createMutableAttribute(Attributes.MAX_HEALTH, 12.0D)
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.25D);
     }
 
     @Nullable
--- src/main/java/net/earthspawn/mod/utils/ClientEventBusSubscriber.java
+++ src/main/java/net/earthspawn/mod/utils/ClientEventBusSubscriber.java
@@ -1,12 +1,16 @@
 package net.earthspawn.mod.utils;
 
 import net.earthspawn.mod.EarthspawnMod;
+import net.earthspawn.mod.entities.TestEntity;
 import net.earthspawn.mod.entities.renderer.TestEntityRenderer;
+import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(modid = EarthspawnMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ClientEventBusSubscriber {
@@ -14,5 +18,12 @@ public class ClientEventBusSubscriber {
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event){
         RenderingRegistry.registerEntityRenderingHandler(RegistryHandler.TEST_ENTITY.get(), TestEntityRenderer::new);
+    }
+
+    @SubscribeEvent
+    public static void entityAttributesSetup(final FMLCommonSetupEvent event){
+        DeferredWorkQueue.runLater(() -> {
+            GlobalEntityTypeAttributes.put(RegistryHandler.TEST_ENTITY.get(), TestEntity.setCustomAttributes().create());
+        });
     }
 }
