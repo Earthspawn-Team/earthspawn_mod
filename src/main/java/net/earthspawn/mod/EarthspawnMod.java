28958ebfb9872bea07dda9b8b3b59dfdbdf493d2
Earthspawn <earthspawn.team@gmail.com>
2021-10-03 12:11:44 +0200

    Chipser

--- src/main/java/net/earthspawn/mod/EarthspawnMod.java
+++ src/main/java/net/earthspawn/mod/EarthspawnMod.java
@@ -1,15 +1,9 @@
 package net.earthspawn.mod;
 
-import net.earthspawn.mod.entities.TestEntity;
 import net.earthspawn.mod.utils.RegistryHandler;
-import net.minecraft.entity.ai.attributes.GlobalEntityTypeAttributes;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DeferredWorkQueue;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
